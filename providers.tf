363c5866fd844d8491b5b28c237697422f6232da
atpk2266 <82212964+atpk2266@users.noreply.github.com>
2021-06-17 16:47:42 +0530

    Create providers.tf

---
+++ providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  # This module is now only being tested with Terraform 0.14.x. However, to make upgrading easier, we are setting
+  # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
+  # forwards compatible with 0.14.x code.
+  
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.43.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 3.43.0"
+    }
+  }
+}
+
+# ------------------------------------------------------------------------------
+# CONFIGURE OUR GCP CONNECTION
+# ------------------------------------------------------------------------------
+
+provider "google" {
+  region  = var.region
+  project = var.project
+}
+
+provider "google-beta" {
+  region  = var.region
+  project = var.project
+}
